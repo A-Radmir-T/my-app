ee91676cbd1e98cd2b36431b1b00e0753f4e6500
Your Nameart555w <art555m@gmail.com>
2023-08-02 19:31:39 +0500

    rewrote without jsx App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import logo from './logo.svg'
 import './App.css'
+import React from 'react'
 
 export const App = () => {
 	const currentYear = new Date().getFullYear() // декларативный
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
-	) // декларативный
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				/*#__PURE__*/ React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, currentYear),
+		),
+	)
 }
